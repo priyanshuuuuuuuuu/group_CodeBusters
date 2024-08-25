cd305208e75eebb962452fcae4a413476219600d
solardroids <146615961+solardroids@users.noreply.github.com>
2024-08-25 13:34:12 +0530

    Update script.js

--- Hack'24/CodeBusters/Our Team/script.js
+++ Hack'24/CodeBusters/Our Team/script.js
@@ -1,9 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const welcomeText = document.getElementById("welcome-text");
-    const headline = document.getElementById("headline");
-
-    // Wait for the typewriter animation to end before showing the headline
-    welcomeText.addEventListener('animationend', function() {
-        headline.style.opacity = 1; // Make the headline visible
-    });
-});
+function showTime() {
+	document.getElementById('currentTime').innerHTML = new Date().toUTCString();
+}
+showTime();
+setInterval(function () {
+	showTime();
+}, 1000);
